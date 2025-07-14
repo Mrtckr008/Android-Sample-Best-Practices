0666a4c4d6fe3e43ba10cc1c416eaaf3aa5d1663
d-mcakir <d-mcakir@ziraatteknoloji.com>
2025-07-14 23:46:14 +0300

    Coverage library "Kover" is integrated by following document https://kotlin.github.io/kotlinx-kover/gradle-plugin/

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,12 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.compose.screenshot)
     alias(libs.plugins.toolchains) apply false
+    id("org.jetbrains.kotlinx.kover") apply false
+}
+
+subprojects {
+    plugins.withId("com.android.application")    { apply(plugin = "org.jetbrains.kotlinx.kover") }
+    plugins.withId("com.android.library")        { apply(plugin = "org.jetbrains.kotlinx.kover") }
+    plugins.withId("org.jetbrains.kotlin.jvm")   { apply(plugin = "org.jetbrains.kotlinx.kover") }
+    plugins.withId("org.jetbrains.kotlin.android"){ apply(plugin = "org.jetbrains.kotlinx.kover") }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
